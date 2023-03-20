d9af4afa4b7b2b96464a64454eb479a60b34e8b1
monikeshari <monikeshari0715@gmail.com>
2023-03-20 19:10:45 +0530

    homepage creating

---
+++ src/app/about/page.tsx
@@ -0,0 +1,37 @@
+
+
+import styles from './about.module.scss'
+
+const About = () => {
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.card}>
+        <div className={styles.title}>
+          Card 1
+        </div>
+        <div className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </div>
+      </div>
+
+      <div className={styles.card}>
+        <div className={styles.title}>
+          Card 2
+        </div>
+        <div className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </div>
+      </div>
+
+      <div className={styles.card}>
+        <div className={styles.title}>
+          Card 3
+        </div>
+        <div className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </div>
+      </div>
+    </div>
+  )
+}
+export default About;
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+
+
+import Header from './header/header'
+
+
+
+export default function Home() {
+
+  return (
+    <main>
+
+      <Header />
+
+    </main>
+  )
+}
