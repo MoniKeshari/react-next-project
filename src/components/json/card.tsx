1022b86eca719ce891307acc1d672c7712dbfce8
monikeshari <monikeshari0715@gmail.com>
2023-03-28 00:49:46 +0530

    add cart images

--- src/components/json/card.tsx
+++ src/components/json/card.tsx
@@ -4,53 +4,88 @@ import {cardArray} from '../../../src/types/hometype'
 const location: cardArray = [
     {
         id: 1,
-        img: "./homeimg/img5.jpg",
-        name: 'Tazmahal',
-        desc: 'It is beautiful location'
+        img: "./homeimg/wc4.jpg",
+        name: 'women one piece dress',
+        desc: 'It is smooth and comfortable dress'
 
     },
     {
         id: 2,
-        img: './homeimg/img6.jpg',
-        name: 'Tazmahal',
-        desc: 'It is beautiful location'
+        img: './homeimg/wc.jpg',
+        name: 'women one piece dress',
+        desc: 'It is smooth and comfortable dress'
 
     },
     {
         id: 3,
-        img: './homeimg/img7.jpg',
-        name: 'Tazmahal',
-        desc: 'It is beautiful location'
+        img: './homeimg/w489.jpg',
+        name: 'women one piece dress',
+        desc: 'It is smooth and comfortable dress'
 
     },
     {
         id: 4,
-        img: './homeimg/img8.jpg',
-        name: 'Tazmahal',
-        desc: 'It is beautiful location'
-
+        img: './homeimg/w78.jpg',
+        name: 'women one piece dress',
+        desc: 'It is smooth and comfortable dress'
     },
     {
         id: 5,
-        img: './homeimg/img9.jpg',
-        name: 'Tazmahal',
-        desc: 'It is beautiful location'
+        img: './homeimg/we.jpg',
+        name: 'Earning Jhumka',
+        desc: 'It is nice product'
 
     },
     {
         id: 6,
-        img: './homeimg/img10.jpg',
-        name: 'Tazmahal',
-        desc: 'It is beautiful location'
+        img: './homeimg/w2.jpg',
+        name: 'Summer T-Shirt',
+        desc: 'It is smooth and comfortable dress'
 
     },
     {
         id: 7,
-        img: './homeimg/img3.jpg',
-        name: 'Tazmahal',
-        desc: 'It is beautiful location'
+        img: './homeimg/b0.jpg',
+        name: 'Girl Red Frok',
+        desc: 'It is smooth and comfortable dress'
 
     },
+    {
+        id: 8,
+        img: './homeimg/w489.jpg',
+        name: 'women one piece dress',
+        desc: 'It is smooth and comfortable dress'
+
+    },
+    {
+        id: 9,
+        img: './homeimg/w1.jpg',
+        name: 'White Watch',
+        desc: 'It is beautiful Watch'
+
+    },
+    {
+        id: 10,
+        img: './homeimg/b8.jpg',
+        name: 'Baby Frok',
+        desc: 'It is beautiful Frok'
+
+    },
+    {
+        id: 11,
+        img: './homeimg/w4.jpg',
+        name: 'Women Braslet',
+        desc: 'It is beautiful Braslet'
+
+    },
+    {
+        id: 12,
+        img: './homeimg/watches1.jpg',
+        name: 'Women Watch',
+        desc: 'It is nice Watch'
+
+    },
+   
 
 ]
 export default location
--- src/components/locationcard/cardSummary.tsx
+++ src/components/locationcard/cardSummary.tsx
@@ -1,19 +1,25 @@
 import { cardType } from '@/types/hometype';
 import React from 'react';
 import styles from '../locationcard/cardSummary.module.scss';
+import { Button } from 'react-bootstrap';
 const CardSummary = (props: cardType) => {
     const { img, desc, name } = props;
     return (
         <div className='col-md-6 col-lg-3 mx-0 mb-4'>
             <div className={`${styles.cardData} card p-0 overflow-hidden shadow h-100`}>
-                <img src={img} alt=''/>
-               
+                <img src={img} alt='' />
+
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text">{desc}</p>
                 </div>
-                <div className="card-footer">
-                    <small className="text-muted">{desc}</small>
+                <div className={`${styles.cardFooter} card-footer`}>
+                    <small className="text-muted">
+                        <Button className={styles.dec}>-</Button>
+                               
+                        <Button className={styles.inc}>+</Button>
+
+                    </small>
                 </div>
             </div>
 
