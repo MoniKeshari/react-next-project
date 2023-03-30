c8c896c917098cfada619673bbb8737c439fe958
monikeshari <monikeshari0715@gmail.com>
2023-03-30 15:18:10 +0530

    cartdetails ui make

--- src/pages/cartdetails.tsx
+++ src/pages/cartdetails.tsx
@@ -1,32 +1,58 @@
-import { Button } from 'react-bootstrap';
-import styles from '../components/locationcard/cardSummary.module.scss'
 
-const CartDetail = ({ item }: any) => {
+import styles from '../pages/cartdetail.module.scss';
+import Image from 'next/image';
+
+const CartDetail = ({ itemCount }: {itemCount:number}) => {
+    console.log(itemCount, 'itemdata');
+    
 
     return (
         <>
-            <div className='col-md-6 col-lg-3 mx-0 mb-4'>
-                <div className={`${styles.cardData} card p-0 overflow-hidden shadow h-100`}>
-                    <img src={item?.img} alt='' />
-
-                    <div className="card-body">
-                        <h5 className="card-title">{item?.name}</h5>
-                        <p className="card-text">Price:{item?.price}</p>
-                        <p className="card-text">{item?.desc}</p>
-                    </div>
-                    <div className={`${styles.cardFooter} card-footer`}>
-                        <small className="text-muted">
-
-                            <Button >back</Button>
-
-                        </small>
-                    </div>
+            <div className={`${styles.wrapper} row justify-content-center`}>
+                <div className={`${styles.imgpic} col-md-2`}>
+                    <Image
+                        src="/homeimg/about.svg"
+                        alt="Description of the image"
+                        width={200}
+                        height={200}
+                    />
                 </div>
 
+                <div className='col-md-6 mt-5'>
+                    <div className='row justify-content-center'>
+
+                        <div className={`${styles.containerData} col-md-4 `}>
+                            <ul>
+                                <li>
+                                    <h6>name :{itemCount}</h6>
+                                </li>
+                                <li>
+                                    <p>price</p>
+                                </li>
+                                <li>
+                                    <p>desc</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='col-md-2 '> <button>dec</button></div>
+                        <div className='col-md-2 '>
+
+                            <p>count</p>
+
+                        </div>
+                        <div className='col-md-2 '> <button>dec</button></div>
+
+
+
+                    </div>
+
+
+                </div>
+                
 
             </div>
-           
         </>
     )
 }
+
 export default CartDetail;
