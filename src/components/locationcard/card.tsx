a80cb235141931ebe9528dde6530e254b591ae33
monikeshari <monikeshari0715@gmail.com>
2023-04-04 10:39:14 +0530

    changes

--- src/components/locationcard/card.tsx
+++ src/components/locationcard/card.tsx
@@ -1,24 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import CardSummary from './cardSummary';
 import { CardItem } from '@/types/hometype';
-import CardShimmer from './cardSummuryShimmer';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import axios from 'axios';
 
 const CardData = () => {
-    const [isLoading, setLoading] = useState(true);
+  
     const [product, setProduct] = useState<CardItem[]>([]);
 
     useEffect(() => {
-        setTimeout(() => {
+       
             axios.get('https://fakestoreapi.com/products').then((res: any) => {
                 const response = res.data;
                 setProduct(response);
-                setLoading(false);
+              
 
 
             })
-        }, 2000)
+      
 
     }, [])
 
@@ -33,7 +32,7 @@ const CardData = () => {
                             return (
                                 <>
 
-                                    {isLoading ? (<CardShimmer />) : (<CardSummary item={item} />) }
+                                <CardSummary item={item} />
 
                                 </>
 
