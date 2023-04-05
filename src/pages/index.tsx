fb7391f7bca212e53f376617ebd4228fb491f119
monikeshari <monikeshari0715@gmail.com>
2023-04-05 11:18:47 +0530

    chnges

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ const Home = () => {
     }, [])
     return (
         <>
-            <div className={styles.wrapper}>
+            <div>
                 {/* <LocationArea /> */}
                 {/* <Search /> */}
                 < ControlledCarousel />
