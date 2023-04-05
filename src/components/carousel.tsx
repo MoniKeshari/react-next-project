50c7b1a723bc66ad42e4526d1bbc3db4edb6ceb7
monikeshari <monikeshari0715@gmail.com>
2023-04-05 12:19:35 +0530

    carousel implemented

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,37 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './carousel.module.scss';
-import Carousel from 'react-bootstrap/Carousel';
+// import Carousel from 'react-bootstrap/Carousel';
 import carousel from '../components/json/carousel';
-const ControlledCarousel = () => {
-  const [index, setIndex] = useState(0);
 
-  const handleSelect = (selectedIndex: any, e: any) => {
-    setIndex(selectedIndex);
+// import Carousel from 'react-multi-carousel';
+// import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carousel styles
+import { Carousel } from "react-responsive-carousel"; // Import carousel component
+const ControlledCarousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const interval = 2000;
+  const handleChange = (index: number) => {
+    setCurrentIndex(index);
   };
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // increment current index and loop back to 0 when end of carousel is reached
+      setCurrentIndex(currentIndex => (currentIndex + 1));
+    }, interval);
+
+    // clear interval on component unmount
+    if (currentIndex) {
+      setTimeout(() => setCurrentIndex(0), interval);
+    }
+
+    // clear interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []); 
   return (
     <>
-    <div className={styles.container}>
-    {carousel.map((id) => {
-        return (
-          <><Carousel className={styles.Carouseldata} activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item className={styles.carouselItem}>
-              <img
-                className="d-block w-100"
-                src={`/${id.imageUrl}`}
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>{id.id}</h3>
-                <p>{id.title}</p>
-              </Carousel.Caption>
-            </Carousel.Item>
+      <div className={styles.container}>
+      <Carousel 
+        selectedItem={currentIndex} 
+        onChange={handleChange}
+      >
+        <div>
+          <img src='./homeimg/other2.jpg' alt="Slide 1" />
+        </div>
+        <div>
+          <img src='./homeimg/other.jpg' alt="Slide 2" />
+        </div>
+        <div>
+          <img src="./homeimg/bag.jpg" alt="Slide 3" />
+        </div>
+      </Carousel>
 
-          </Carousel></>
-        )
-      })}
+      {/* <button onClick={handlePrevClick}>Previous</button>
+      <button onClick={handleNextClick}>Next</button> */}
     </div>
-    
 
     </>
 
